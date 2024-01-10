f1ed9959226643499c5a7e191ec28bb6a14f1ed5
Mohamed <123892930+Mohoua93@users.noreply.github.com>
2024-01-10 15:28:50 +0100

    Ajout des commentaires

--- js/product.js
+++ js/product.js
@@ -1,22 +1,28 @@
+// Récupération de l'ID du produit depuis les paramètres de l'URL
 const params = new URL(document.location).searchParams;
 const id = params.get("id");
 
+// Construction de l'URL pour récupérer les détails d'un produit spécifique depuis l'API
 const url = `http://localhost:3000/api/products/${id}`;
 
+// Fonction pour obtenir et afficher les détails d'un produit spécifique
 const getArticle = () => {
   fetch(url)
     .then(function (res) {
-      return res.json();
+      return res.json(); // Conversion de la réponse en JSON
     })
     .then(function (data) {
-      const addTitle = (document.getElementById("title").innerHTML = data.name);
-      const addPrice = (document.getElementById("price").innerHTML =
-        data.price);
-      const addDescription = (document.getElementById("description").innerHTML =
-        data.description);
+      // Mise à jour du DOM avec les détails du produit récupérés depuis l'API
+      document.getElementById("title").innerHTML = data.name;
+      document.getElementById("price").innerHTML = data.price;
+      document.getElementById("description").innerHTML = data.description;
+
+      // Ajout de l'image du produit
       const addImg = document.createElement("img");
       document.querySelector(".item__img").appendChild(addImg);
       addImg.setAttribute("src", `${data.imageUrl}`);
+
+      // Ajout des options de couleur du produit dans un menu déroulant
       const addOption = document.getElementById("colors");
       for (color in data.colors) {
         addOption.innerHTML += `<option value="${data.colors[color]}">${data.colors[color]}</option>`;
@@ -24,13 +30,17 @@ const getArticle = () => {
     });
 };
 
+// Gestion de l'événement de clic sur le bouton "Ajouter au panier"
 const btnAddToCart = document.getElementById("addToCart");
 
 btnAddToCart.addEventListener("click", () => {
+  // Récupération de la quantité et des options de couleur choisies par l'utilisateur
   const quantityInput = document.getElementById("quantity");
   const quantityValue = parseInt(quantityInput.value, 10);
 
+  // Vérification si la quantité est valide (entre 0 et 100)
   if (quantityValue >= 0 && quantityValue <= 100) {
+    // Création d'un objet représentant le produit à ajouter au panier
     const addProduct = {
       quantity: quantityValue,
       colors: document.getElementById("colors").value,
@@ -39,6 +49,7 @@ btnAddToCart.addEventListener("click", () => {
 
     let addProductLocalStorage;
 
+    // Gestion du stockage local pour sauvegarder les produits ajoutés au panier
     if (localStorage.getItem("addToCart") !== null) {
       addProductLocalStorage = JSON.parse(localStorage.getItem("addToCart"));
       addProductLocalStorage.push(addProduct);
@@ -49,8 +60,10 @@ btnAddToCart.addEventListener("click", () => {
       localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage));
     }
   } else {
+    // Alerte si la quantité n'est pas valide
     alert("La quantité doit être comprise entre 0 et 100.");
   }
 });
 
+// Appel de la fonction pour récupérer et afficher les détails du produit au chargement de la page
 getArticle();
--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-const url =  "http://localhost:3000/api/products"
-const container = document.getElementById("items")
+// Déclaration de l'URL de l'API et récupération de l'élément HTML où les produits seront affichés
+const url = "http://localhost:3000/api/products";
+const container = document.getElementById("items");
 
+// Fonction pour récupérer les articles depuis l'API
 const getArticles = () => {
-    fetch(url)
+  // Appel à l'API avec fetch
+  fetch(url)
     .then(function (res) {
-        return res.json()
+      return res.json(); // Conversion de la réponse en JSON
     })
-    .then (function (data) {
-  
-        for(product in data){
-          console.log(data[product]._id, 'mes canapés')
-            container.innerHTML += `<a href="./product.html?id=${data[product]._id}">
+    .then(function (data) {
+      // Une fois les données récupérées avec succès, itération à travers chaque produit
+      for (const product of data) {
+          
+         // Création de l'élément HTML pour chaque produit et ajout au conteneur
+          container.innerHTML += `<a href="./product.html?id=${product._id}">
             <article>
-              <img src="${data[product].imageUrl}" alt=${data[product].altTxt}">
-              <h3 class="productName">"${data[product].name}"</h3>
-              <p class="productDescription">"${data[product].description}</p>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
             </article>
-          </a>`
-        }
-    })
-}
+          </a>`;
+        //}
+      }
+    });
+};
 
-getArticles()
+// Appel de la fonction pour récupérer et afficher les articles au chargement de la page
+getArticles();
